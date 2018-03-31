933b87523f88bcbb36bdfa67dbaf476b24dbfac0
Ash Bhimasani <16abhimasani@gmail.com>
2018-03-30 21:42:12 -0400

    Trying to fix problems

--- template/plugins/vue-awesome.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import Icon from 'vue-awesome';
-const VueAwesome = {
-  install(Vue, options) {
-    Vue.component('icon', Icon)
-  }
-};
-Vue.use(VueAwesome);
-export default VueAwesome;
